07c4ff931adb9ca8e7f7307d3ef7446456fe365f
SergeySavastyuk <ortariy@tut.by>
2024-05-09 20:56:08 +0300

    09.05.2024 20/55

--- 0.py
+++ 0.py
@@ -1,6 +1,1 @@
 # -*- coding: UTF-8 -*-
-# Функция create_matrix должна возвращать квадратную матрицу размером size х size, на диагонали которой располагаются
-# числа от 1 до size. Все остальные элементы заполнены согласно параметрам up_fill и down_fill.
-def create_matrix(size:int = 3,up_fill: int = 0, down_fill:int = 0):
-    return [[up_fill if row < column else (down_fill, row+1)[row == column] for column in range(size)] for row in range(size)]
-print(create_matrix(size=4, up_fill=7, down_fill=9))
--- Indi-kyrs/9.3 Работаем с JSON.py
+++ Indi-kyrs/9.3 Работаем с JSON.py
@@ -1,9 +1,9 @@
 """Процесс сериализации в JSON-формат заключается в преобразовании питоновского объекта(обычно словаря,
-но можно использовать и другие типы) в строку, сформированную по правилам JSON-формата. Например, такой вот словарь"""
+но можно использовать и другие типы) в строку, сформированную по правилам JSON-формата. Например, такой вот словарь
 {
  'apple': {"price": 10,"color": "green"},
  'banana': {"price": 4.5, 'color': "yellow"},
-}
+} """
 """будет преобразован вот в такую строку """
 '{"apple": {"price": 10, "color": "green"}, "banana": {"price": 4.5, "color": "yellow"}}'
 # Сериализацию можно использовать для хранения объектов на диске,
@@ -24,8 +24,10 @@ import json
 функция load десериализует файл, хранящий JSON-строку, в питоновский объект"""
 
 import json
+import datetime
 from random import randint
-str_json = """
+
+str_json = """ 
 {
     "response": {
         "count": 32363,
@@ -45,27 +47,44 @@ str_json = """
         ]
     }
 }
-"""
-data = json.loads(str_json)
-for item in data["response"]["items"]:
+""" # здесь храниться данные из json файла
+data = json.loads(str_json) # превращает строку в словарь с которым можно работать
+for item in data["response"]["items"]:  # перебираем по нужному ключу
     del item["id"]
     item["likes"] = randint(100, 200)
-new_json = json.dumps(data)
+    item['a'] = None
+    item['b'] = True
+    item['now'] = datetime.datetime.now().strftime("%d/%m/%y") # функции strftime - преобразует формат времени в строку
+new_json = json.dumps(data)  # вернём обратно словарь в формат json для загрузки
 print(new_json)
+
 print('Ниже json с параметром indent'.center(40, '-'))
 # Чтобы строка сформировалась в более красивом виде можно задать значение отступа в параметре indent функции dumps:
 json_indent = json.dumps(data, indent = 2)
 print(json_indent)
-print('-'*40)
+print('-'*60)
+print()
 
 # Обратите внимание на нижние значения:
 # питоновское значение None будет сконвертировано в значение null в формате JSON
 # питоновское значение True будет сконвертировано в значение true в формате JSON
 # питоновское значение False будет сконвертировано в значение false в формате JSON
+""" объекты преобразования
+Python	                JSON
+dict	                object
+list, tuple	            array
+str         	        string
+int, long, float	    number
+True	                true
+False	                false
+None	                null
+"""
+
+print('ниже добавление и удаление данных из файла')
 import json
-import datetime
+
 from random import randint
-str_json = """
+str_json2 = """
 {
     "id": 287350527,
     "first_name": "Sonya",
@@ -73,7 +92,7 @@ str_json = """
     "photo_50": "https://pp.vk.me/...2c1/J2EL--qCZa8.jpg"
 }
 """
-data = json.loads(str_json)
+data = json.loads(str_json2)
 del data["photo_50"]
 data["likes"] = randint(100, 200)
 data["is_married"] = False
@@ -81,15 +100,11 @@ data["is_adult"] = True
 data["mail"] = None
 json_indent = json.dumps(data, indent=2)
 print(json_indent)
+print(data, ' - тот же файл но без indent')
+print(type(data), ' - тип нашего фала')
+print()
 
-for item in data["response"]["items"]:
-    del item["id"]
-    item["likes"] = randint(100, 200)
-    item['a'] = None
-    item['b'] = True
-    item['now'] = datetime.now().strftime("%d/%m/%y") # функции strftime - преобразует формат времени в строку
-
-# Часто JSON необходимо сохранять в виде файла.
+'''Часто JSON необходимо сохранять в виде файла.'''
 # Для этого в json есть метод dump, который принимает 2 аргумента: 1 – объект, который хотите сохранить,
 # 2 – название файлика. Для этого воспользуемся инструкцией with.
 with open("my.json", "w") as file:
@@ -102,18 +117,20 @@ with open("my.json", "w") as file:
 # И раз мы можем сохранять это в файл, то мы сможем и забирать из него. Откроем его для чтения в чистом файле:
 with open("my.json", "r") as file:
     json.load(file)
-print(data)
-print(type(data))
+"""удалим файл чтобы не мешал в дереве   """
+import os
+os.remove('my.json')
 
-"""выполнить сериализацию словаря"""
+print('Задания'.center(60,'-'))
+print('---№1 Выполнить сериализацию словаря, дана строка латинских букв, сгенерируем словарь')
 import json
 from string import ascii_lowercase  # строка латинских букв
 alphabet = {i:ascii_lowercase.index(i)+1 for i in ascii_lowercase} # сгенерируем словарь, где ключ - буква, значение - индекс в строке
 json_data = json.dumps(alphabet) # преобразуем в JSON-строку
 print(json_data)
+print()
 
-
-""" Распечатайте информацию о каждом человеке """
+print('---№2 Распечатайте информацию о каждом человеке')
 people = '[{"name": "Haley Whitney", "country": "British Indian Ocean Territory (Chagos Archipelago)", "age": 54}, ' \
          '{"name": "Matthew King", "country": "Colombia", "age": 34}, ' \
          '{"name": "Sean Sullivan", "country": "Mayotte", "age": 40}, ' \
